f8e485a2d2dec628f09fb6c4a775ea853ef2f21d
nonexistentware <nonexistentware@gmail.com>
2025-02-02 23:43:56 +0200

    homework3

---
+++ Homework5.1/homework3.js
@@ -0,0 +1,11 @@
+function pow (x, n) {
+    let result = x;
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+    return result;
+};
+
+const result = pow(2, 3);
+
+console.log(result);
