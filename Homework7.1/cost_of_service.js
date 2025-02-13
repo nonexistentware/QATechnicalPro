68436de065f863d53c4c53eacc71b7b616e253a1
nonexistentware <nonexistentware@gmail.com>
2025-02-13 18:52:59 +0200

    changed methods, added this keyword

--- Homework7.1/cost_of_service.js
+++ Homework7.1/cost_of_service.js
@@ -1,52 +1,54 @@
-const priceList =  {
+const priceList = {
     "Еспрессо" : "10 грн",
     "Американо" : "20 грн",
-    "Лате" : "30 грн"
-}
-priceList["Капучино"] = "40 грн";
+    "Лате" : "30 грн",
+    "Капучино" : "40 грн",
+    
+    sumPrice: function() {
+        let sum = 0;
+        for (let key in this) {
+            if (typeof this[key] === 'string' && this[key].endsWith('грн')) {
+                sum += parseInt(this[key]);
+            }
+        }
+        return sum;
+    },
 
+    minPrice: function() {
+        const priceArray = Object.entries(this).filter(([key, value]) => 
+            typeof value === 'string' && value.endsWith('грн')
+        );
+        const minItem = priceArray.reduce((min, current) => {
+            return parseInt(current[1]) < parseInt(min[1]) ? current : min;
+        });
 
-console.log("Об`кт містить: " + priceList); 
+        return `${minItem[0]}  ${minItem[1]}`;
+    },
 
-// сума вартості всіх властивостей об'єкта
-function sumPrice(priceList) {
-    let sum = 0;
-    for (let key in priceList) {
-        sum += parseInt(priceList[key]);
+    maxPrice: function() {
+        const priceArray = Object.entries(this).filter(([key, value]) => 
+            typeof value === 'string' && value.endsWith('грн')
+        );
+        const maxItem = priceArray.reduce((max, current) => {
+            return parseInt(current[1]) > parseInt(max[1]) ? current : max;
+        });
+
+        return `${maxItem[0]}  ${maxItem[1]}`;
     }
-    return sum;
 };
 
-// мінімальна вартість властивостей об'єкта
-function minPrice(priceList) {
-    const priceArray = Object.entries(priceList);
-    const minItem = priceArray.reduce((min, current) => {
-        return parseInt(current[1]) < parseInt(min[1]) ? current : min;
-    });
+priceList["Мокачино"] = "50 грн";  // Нова властивість
 
-    return `${minItem[0]}  ${minItem[1]}`;
-}
-
-// максимальна вартість властивостей об'єкта
-function maxPrice(priceList) {
-    const priceArray = Object.entries(priceList);
-    const minItem = priceArray.reduce((min, current) => {
-        return parseInt(current[1]) > parseInt(min[1]) ? current : min;
-    });
-
-    return `${minItem[0]}  ${minItem[1]}`;
-} 
+console.log("Об`єкт містить: " + JSON.stringify(priceList));
 
 // сума вартості всіх властивостей об'єкта
-const sum = sumPrice(priceList);
-console.log("Загльна сума властивостей об'єкта: " + Object.keys(priceList)+ " " + sumPrice(priceList) + " грн");
-
+const sum = priceList.sumPrice();
+console.log("Загльна сума властивостей об'єкта: " + sum + " грн");
 
 // мінімальна вартість властивостей об'єкта
-const min = minPrice(priceList);
+const min = priceList.minPrice();
 console.log("Мінімальна вартість: " + min);
 
 // максимальна вартість властивостей об'єкта
-const max = maxPrice(priceList);
+const max = priceList.maxPrice();
 console.log("Максимальна вартість: " + max);
- 
