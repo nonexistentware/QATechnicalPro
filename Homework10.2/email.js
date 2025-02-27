15e105d6cc35f0b2faea146ac5250e7eb44abde1
nonexistentware <nonexistentware@gmail.com>
2025-02-27 19:19:50 +0200

    added validation before @ in the email

--- Homework10.2/email.js
+++ Homework10.2/email.js
@@ -12,14 +12,27 @@ let arr = [
     {
         userName:"Andrii",
         lastName:"",
-        email:"andrii@mail.ru" // Нам такі не підходять
+        email:"andrii@mail.ru" 
     },
+
+    {
+        userName:"Ivan", //тестовий кастомер з невалідним імейлом 
+        lastName:"testuser",
+        email:"@#%@#5 #hgjtl.com"
+    },
+
+    {
+        userName:"Customer11", //тестовий кастомер з невалідним імейлом 
+        lastName:"Test999",
+        email:"імейл@gmail.com"
+    }
 ];
 
 function checkEmail(arr) {
     const pattern = /@gmail|@yahoo/;
+    const pattern2 = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/; //додав валідацію для перевірки імейлу перед @
     arr.forEach(user => {
-        if (user.email.match(pattern)) {
+        if (user.email.match(pattern) && user.email.match(pattern2)) {
             console.log(`Email ${user.email} is valid`);
         } else {
             console.log(`Email ${user.email} is not valid`);
